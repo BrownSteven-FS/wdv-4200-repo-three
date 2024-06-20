a3cbcf175e04af89ce5de05ab547b214385c9b2c
Steven Brown <smbrown1@student.fullsail.edu>
2024-06-20 07:38:40 -0500

    Add try catch block so application will error out if env vars aren't set properly.

--- app.js
+++ app.js
@@ -3,38 +3,46 @@ require("dotenv").config("./.env");
 const SECRET_QUESTION = process.env.SECRET_QUESTION;
 const SECRET_ANSWER = process.env.SECRET_ANSWER;
 
-const http = require("http");
-const hostname = "127.0.0.1";
-const port = 3000;
+try {
+  if (SECRET_ANSWER === undefined || SECRET_QUESTION === undefined)
+    throw new Error(
+      "You need the SECRET_QUESTION and the SECRET_ANSWER for this application to run!"
+    );
+  const http = require("http");
+  const hostname = "127.0.0.1";
+  const port = 3000;
 
-const fs = require("fs");
-const directoryName = "./";
-const filenames = fs.readdirSync(directoryName);
+  const fs = require("fs");
+  const directoryName = "./";
+  const filenames = fs.readdirSync(directoryName);
 
-const readline = require("readline");
+  const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-
-  let fileContents = "";
-  filenames.forEach((file) => {
-    fileContents += file + "\n";
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  res.end(fileContents);
-});
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    let fileContents = "";
+    filenames.forEach((file) => {
+      fileContents += file + "\n";
+    });
 
-  rl.question(SECRET_QUESTION, () => {
-    console.log(SECRET_ANSWER);
-    rl.close();
+    res.end(fileContents);
   });
-});
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+
+    rl.question(SECRET_QUESTION, () => {
+      console.log(SECRET_ANSWER);
+      rl.close();
+    });
+  });
+} catch (e) {
+  console.error(e);
+}
